0beec23c4f608803844f3f3176448d1786a183d8
Pintu <87765219+PintuDebNath@users.noreply.github.com>
2022-12-13 13:24:30 +0530

    Update and rename Main.py to App.py

--- App.py
+++ App.py
@@ -64,5 +64,5 @@ def armstrong(string):
         id = id+1
     return finaldata
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="5000")
+    app.run(debug=True, host="0.0.0.0", port="8080")
 
